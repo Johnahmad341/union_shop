eeaffd9c13a7b78150901c3542dfe386908ba4ac
Johnahmad341 <johnahmad341@gmail.com>
2025-12-03 18:33:17 +0000

    Add route for AllProductsPage in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:union_shop/src/pages/collections_page.dart';
 import 'package:union_shop/src/pages/collection_detail_page.dart';
 import 'package:union_shop/src/pages/sale_collection_page.dart';
 import 'package:union_shop/src/pages/auth_page.dart';
+import 'package:union_shop/src/pages/all_products_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -30,6 +31,7 @@ class UnionShopApp extends StatelessWidget {
         '/collection-detail': (context) => const CollectionDetailPage(),
         '/sale-collection': (context) => const SaleCollectionPage(),
         '/auth': (context) => const AuthPage(),
+        '/all-products': (context) => const AllProductsPage(),
       },
     );
   }
