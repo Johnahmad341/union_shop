dd4558f85520a592667606a23bafec9c6c3707f8
Johnahmad341 <johnahmad341@gmail.com>
2025-12-03 10:39:43 +0000

    Add route for SaleCollectionPage in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:union_shop/src/pages/home_page.dart';
 import 'package:union_shop/src/pages/product_page.dart';
 import 'package:union_shop/src/pages/collections_page.dart';
 import 'package:union_shop/src/pages/collection_detail_page.dart';
+import 'package:union_shop/src/pages/sale_collection_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -26,6 +27,7 @@ class UnionShopApp extends StatelessWidget {
         '/about': (context) => const AboutUsPage(),
         '/collections': (context) => const CollectionsPage(),
         '/collection-detail': (context) => const CollectionDetailPage(),
+        '/sale-collection': (context) => const SaleCollectionPage(),
       },
     );
   }
