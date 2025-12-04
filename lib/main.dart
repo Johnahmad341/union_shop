bd08e72457986fe43b526e090fc45657f90b7f48
Johnahmad341 <johnahmad341@gmail.com>
2025-12-04 20:40:46 +0000

    Add provider dependency and implement MultiProvider in main.dart for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:union_shop/src/pages/about_us_page.dart';
 import 'package:union_shop/src/pages/home_page.dart';
 import 'package:union_shop/src/pages/product_page.dart';
@@ -7,6 +8,7 @@ import 'package:union_shop/src/pages/collection_detail_page.dart';
 import 'package:union_shop/src/pages/sale_collection_page.dart';
 import 'package:union_shop/src/pages/auth_page.dart';
 import 'package:union_shop/src/pages/all_products_page.dart';
+import 'package:union_shop/src/services/cart_service.dart';
 
 void main() {
   runApp(const UnionShopApp());
@@ -17,22 +19,27 @@ class UnionShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Union Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CartService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Union Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
+        routes: {
+          '/product': (context) => const ProductPage(),
+          '/about': (context) => const AboutUsPage(),
+          '/collections': (context) => const CollectionsPage(),
+          '/collection-detail': (context) => const CollectionDetailPage(),
+          '/sale-collection': (context) => const SaleCollectionPage(),
+          '/auth': (context) => const AuthPage(),
+          '/all-products': (context) => const AllProductsPage(),
+        },
       ),
-      home: const HomeScreen(),
-      routes: {
-        '/product': (context) => const ProductPage(),
-        '/about': (context) => const AboutUsPage(),
-        '/collections': (context) => const CollectionsPage(),
-        '/collection-detail': (context) => const CollectionDetailPage(),
-        '/sale-collection': (context) => const SaleCollectionPage(),
-        '/auth': (context) => const AuthPage(),
-        '/all-products': (context) => const AllProductsPage(),
-      },
     );
   }
 }
